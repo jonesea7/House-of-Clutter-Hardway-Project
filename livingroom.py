885c1db1ede86f1f17895d5c840a2eadd10bc7da
jonesea7 <46021053+jonesea7@users.noreply.github.com>
2019-04-06 08:02:18 -0400

    initial living room function

--- livingroom.py
+++ livingroom.py
@@ -1,1 +1,11 @@
-
+def living_room():
+  print("After you avoid the lego trap, you see what appears to be a dark living room.")
+  print('''The room is full with couches on the north, south, east sides of the room.
+  On the western wall is a large shelf with some old musty books. What will you do:
+  Look around.
+  Sit and rest.
+  Approach the bookshelf.''')
+  
+  if choice == 'look':
+    print('You notice a faint fume coming from the bookshelf, and the couches are clearly too big for the room for you to move around easily.')
+    
