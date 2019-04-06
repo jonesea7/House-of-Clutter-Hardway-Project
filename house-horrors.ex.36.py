4da5d7c451cc84558be6d0e48ea9be304982e2ff
jonesea7 <46021053+jonesea7@users.noreply.github.com>
2019-04-06 08:13:00 -0400

    start function I set up in Visual Code

---
+++ house-horrors.ex.36.py
@@ -0,0 +1,17 @@
+def start():
+    print("""You walk up to the doorstep of an eerie home
+    on the eastside of Detroit. The door is ajar and you push it open.
+    Inside the smell of old shoes assaults your nose. There is a giant wall 
+    directly in front of you. To your left is a small step. Do you:
+    Walk up the step and explore? 
+    or
+    Pull out your cell phone and use it as a flashlight?""")
+
+    choice = input('> ')
+
+    if choice == "Walk" or "explore":
+        print("You step on a pile of legos and trip and break your neck.")
+    elif choice == "look" or "flashlight" or "phone":
+        print("You see a dangerous pile of loose legos on the floor. You decide to proceed with caution.")
+
+start()
