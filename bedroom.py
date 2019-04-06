91ccf8c2ce14a299dd2701f7ae78a9b4e4881f33
jonesea7 <46021053+jonesea7@users.noreply.github.com>
2019-04-06 08:08:22 -0400

    function for bedroom description

---
+++ bedroom.py
@@ -0,0 +1,4 @@
+def bedroom():
+  print("""You open the damaged door and see a room full of clothing from floor to ceiling.
+  The room is musty with the smell of mildew and long abandoned dirty laundry.""")
+  
